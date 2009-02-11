6d307ed8c2248e01db7140a8c8ea898cea9eeda6
Yoshimasa Niwa <niw@niw.at>
2009-02-12 08:54:05 +0900

    Add new ignore, Add git support to the build script

--- build/version.rb
+++ build/version.rb
@@ -5,9 +5,13 @@ require 'rexml/document'
 version = File.read(ARGV.shift).strip
 
 if(src_dir = ARGV.shift)
-  xml = REXML::Document.new(`svn info --xml #{src_dir}`)
-  revision = xml.root.get_elements('entry').first.attributes['revision']
-  puts "#{version}-#{revision}"
-else
-  puts version
+  if File.exist?(File.join(File.expand_path(src_dir), '.svn'))
+    xml = REXML::Document.new(`svn info --xml #{src_dir}`)
+    revision = xml.root.get_elements('entry').first.attributes['revision']
+    version = "#{version}-#{revision}"
+  elsif File.exists?(File.join(File.expand_path(src_dir), '.git'))
+    hash = `cd '#{src_dir}' && git show --pretty='format:%H' HEAD`.split.first
+    version = "#{version}-#{hash}"
+  end
 end
+puts version
